5a46d6964d2e9ed14578d8cde848bb1e4947e8fc
sitamagar <msitanivrutti934@gmail.com>
2024-09-11 15:34:56 +0530

    initial commit by sita

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Chai from "./chaii";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>test para</p>
+      </>
+      
+  );
+}
+
+export default App
---
+++ src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>chai aur biscuit</h3>
+    )
+}
+
+export default Chai
